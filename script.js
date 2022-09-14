beadbaa1379924bf7512d2995d762218da9915db
Hshuttle <huntershutle@gmail.com>
2022-09-14 10:06:41 -0400

    finished for loop to fill out every row and generate icons.  added local storage capabilities and added a save and trash icon to the end on each row

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // Start of for loop to generate timeblocks with correct formatting
+  // Start of for loop to generate time blocks with correct formatting
   for (var i = 8; i <= 22; i++) {
     // sets the color based on time
     var color = "";
@@ -20,5 +20,42 @@ $(document).ready(function () {
     } else {
       hourDisplay = i - 12 + " PM";
     }
+    // row class to set column width
+    var row = $("<section>").addClass("row").attr("id", i);
+    var hourDiv = $("<div>").addClass("col-1 hourDisplay").text(hourDisplay);
+    // text area to type in to do tasks
+    var textArea = $("<textarea>")
+      .addClass("col-9 description " + color)
+      .val(localStorage.getItem(i));
+    //   create save button
+    var saveButton = $("<button>")
+      .addClass("saveBtn col-1")
+      //   saves to local storage
+      .on("click", function () {
+        var hour = $(this).parent().attr("id");
+        var activity = $(this).siblings(".description").val();
+        localStorage.setItem(hour, activity);
+      });
+    // trash button functionality
+    var trashButton = $("<button>")
+      .addClass("saveBtn col-1")
+      //   saves to local storage
+      .on("click", function () {
+        var hour = $(this).parent().attr("id");
+        var emptySpace = $(this).siblings(".description").val("");
+        localStorage.setItem(hour, emptySpace);
+      });
+    //   save saveIcon from font awesome
+    var saveIcon = $("<i>").addClass("fas fa-save");
+    var trashIcon = $("<li>").addClass("fas fa-trash");
+    // adding all variables to the container
+    $(".container").append(
+      row.append(
+        hourDiv,
+        textArea,
+        saveButton.append(saveIcon),
+        trashButton.append(trashIcon)
+      )
+    );
   }
 });
